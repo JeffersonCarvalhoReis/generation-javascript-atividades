f87a19ae9c158389da2e5f05028066e8eb67a7f0
Jefferon Carvalho <jeffersoncarvalhodosreis@gmail.com>
2025-08-26 21:10:50 -0300

    atividade do dia 7

--- dia_7/loops/app/atividade_10.js
+++ dia_7/loops/app/atividade_10.js
@@ -8,7 +8,7 @@ let acc = 0;
 for (let i = 0; i < 10; i++) {
   matriz[i] = [];
   for (let j = 0; j < 4; j++) {
-    matriz[i][j] = leia.questionFloat(`Digite as nota ${j + 1} do aluno ${i + 1}: `);
+    matriz[i][j] = leia.questionFloat(`Digite a nota ${j + 1} do aluno ${i + 1}: `);
     acc += matriz[i][j];
     soma[i] = acc;
   }
